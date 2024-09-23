3cf6e8e343e2b8f99a6711ce79a4afec28d6ded7
Dennis Byberg <dennis.byberg@hotmail.com>
2024-09-23 14:33:38 +0200

    learning

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        Variables();
+    }
+
+    private static void Variables() {
+        // 1. Declare an integer variable (whole number)
+        int number = 10;
+
+        // 2. Declare a double variable (decimal number)
+        float number2 = 10.5f;
+
+        // 3. Declare a boolean variable (true or false)
+        boolean isHere = true;
+
+        // 4. Declare a char variable (single character)
+        char firstLetter = 'd';
+
+        // 5. Declare a String variable (sequence of characters)
+        String name = "Dennis";
+
+        // Print all the variables:
+        System.out.println(number);
+        System.out.println(number2);
+        System.out.println(isHere);
+        System.out.println(firstLetter);
+        System.out.println(name);
+    }
+}
